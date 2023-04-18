c5676adeaefd3a6fc19ab342049ba85ccaf417ca
Geoff Dusome <geoffd@tri.be>
2023-04-18 12:40:11 -0400

    eslint

--- admin/js/block-editor-custom-alignments-admin.js
+++ admin/js/block-editor-custom-alignments-admin.js
@@ -1,38 +1,25 @@
-(function() {
+( function () {
 	'use strict';
 
 	// grab WP variables necessary for the module
-	const {
-		__,
-	} = wp.i18n;
+	const { __ } = wp.i18n;
 
-	const {
-		BlockControls,
-	} = wp.blockEditor;
+	const { BlockControls } = wp.blockEditor;
 
-	const {
-		ToolbarDropdownMenu,
-		ToolbarGroup,
-	} = wp.components;
+	const { ToolbarDropdownMenu, ToolbarGroup } = wp.components;
 
-	const {
-		createHigherOrderComponent,
-	} = wp.compose;
+	const { createHigherOrderComponent } = wp.compose;
 
-	const {
-		getBlockSupport,
-	} = wp.blocks;
+	const { getBlockSupport } = wp.blocks;
 
-	const {
-		Fragment,
-		createElement,
-	} = wp.element;
+	const { Fragment, createElement } = wp.element;
 
 	// add shortcut for creating an element
 	const el = createElement;
 
 	// get alignment from global enqueued theme.json object
-	const themeJSON = tribe.theme_json;
+	// `tribe` comes from a global WP object so ignore eslint here
+	const themeJSON = tribe.theme_json; // eslint-disable-line
 
 	// add object of WP default alignments
 	const defaultWPAlignments = [
@@ -41,22 +28,22 @@
 			slug: 'none',
 			icon: 'M5 15h14V9H5v6zm0 4.8h14v-1.5H5v1.5zM5 4.2v1.5h14V4.2H5z',
 			width: themeJSON.settings.layout.contentSize,
-			textdomain: 'tribe'
+			textdomain: 'tribe',
 		},
 		{
 			name: 'Wide width',
 			slug: 'wide',
 			icon: 'M5 9v6h14V9H5zm11-4.8H8v1.5h8V4.2zM8 19.8h8v-1.5H8v1.5z',
 			width: themeJSON.settings.layout.wideSize,
-			textdomain: 'tribe'
+			textdomain: 'tribe',
 		},
 		{
 			name: 'Full width',
 			slug: 'full',
 			icon: 'M5 4v11h14V4H5zm3 15.8h8v-1.5H8v1.5z',
 			width: '100%',
-			textdomain: 'tribe'
-		}
+			textdomain: 'tribe',
+		},
 	];
 
 	// add object of WP default justifcations
@@ -66,22 +53,22 @@
 			slug: 'left',
 			icon: 'M4 9v6h14V9H4zm8-4.8H4v1.5h8V4.2zM4 19.8h8v-1.5H4v1.5z',
 			width: false,
-			textdomain: 'tribe'
+			textdomain: 'tribe',
 		},
 		{
 			name: 'Align center',
 			slug: 'center',
 			icon: 'M7 9v6h10V9H7zM5 19.8h14v-1.5H5v1.5zM5 4.3v1.5h14V4.3H5z',
 			width: false,
-			textdomain: 'tribe'
+			textdomain: 'tribe',
 		},
 		{
 			name: 'Align right',
 			slug: 'right',
 			icon: 'M6 15h14V9H6v6zm6-10.8v1.5h8V4.2h-8zm0 15.6h8v-1.5h-8v1.5z',
 			width: false,
-			textdomain: 'tribe'
-		}
+			textdomain: 'tribe',
+		},
 	];
 
 	// add array of global blocks to exclude from getting custom support
@@ -96,7 +83,7 @@
 	 * @param {*} settings
 	 * @param {*} name
 	 *
-	 * @returns
+	 * @return {*} object of new settings for the block
 	 */
 	const modifyBlockAlignmentSupport = ( settings, name ) => {
 		// disable specific blocks from having custom alignment support
@@ -112,7 +99,7 @@
 		}
 
 		// Get the original align values from the block supports settings
-		let originalAlignSupport = settings.supports.align;
+		const originalAlignSupport = settings.supports.align;
 
 		// Modify the default block supports settings
 		const newSettings = {
@@ -121,17 +108,21 @@
 				...settings.supports,
 				align: false, // This will disable the default align to hide the core block alignment toolbar
 				alignCustom: originalAlignSupport, // We clone the default align and store it here instead
-			}
+			},
 		};
 
 		return newSettings;
 	};
 
-	wp.hooks.addFilter( 'blocks.registerBlockType', 'tribe/modify-block-align-support', modifyBlockAlignmentSupport );
+	wp.hooks.addFilter(
+		'blocks.registerBlockType',
+		'tribe/modify-block-align-support',
+		modifyBlockAlignmentSupport
+	);
 
 	/*
-	* Function to handle the block alignment icons
-	*/
+	 * Function to handle the block alignment icons
+	 */
 
 	/**
 	 * @function blockAlignIcon
@@ -140,11 +131,17 @@
 	 *
 	 * @param {*} path
 	 *
-	 * @returns
+	 * @return {*} JSX object with the created SVG icon
 	 */
 	const blockAlignIcon = ( path ) => {
-		return el('svg', { className: 'components-menu-items__item-icon', width: 24, height: 24 },
-			el('path', { d: path } )
+		return el(
+			'svg',
+			{
+				className: 'components-menu-items__item-icon',
+				width: 24,
+				height: 24,
+			},
+			el( 'path', { d: path } )
 		);
 	};
 
@@ -153,138 +150,195 @@
 	 *
 	 * @description filter to add custom block alignment toolbar controls
 	 */
-	const customBlockAlignmentControls = createHigherOrderComponent( ( BlockEdit ) => {
-		return ( props ) => {
-			const blockName = props.name;
-			const originalEdit = el( BlockEdit, props );
+	const customBlockAlignmentControls = createHigherOrderComponent(
+		( BlockEdit ) => {
+			return ( props ) => {
+				const blockName = props.name;
+				const originalEdit = el( BlockEdit, props );
 
-			// check for block align support
-			const blockAlignSupport = getBlockSupport( blockName, 'alignCustom', false );
-
-			// disable specific blocks from having custom alignment support
-			if ( excludeBlocksFromCustomAlignmentSupport.includes( blockName ) ) {
-				return originalEdit;
-			}
-
-			// do not add this custom controls if the block type has no align support
-			if ( ! blockAlignSupport ) {
-				return originalEdit;
-			}
-
-			// combine default WP alignments, justications and custom alignments from theme.json
-			const combinedAlignments = [ ...defaultWPAlignments, ...themeJSON.settings._experimentalLayout, ...defaultWPJustifcations ];
-
-			/*
-			* Build the toolbar block alignment controls depening on the align support of the block type
-			*/
-
-			let currentIcon = 'none';
-			const allowedAlignControls = [];
-
-			for ( const alignment of combinedAlignments ) {
-				let {
-					name,
-					slug,
-					icon,
-					width,
-					textdomain
-				} = alignment;
-
-				// check if this alignment is the currently selected alignment for the block and grab the icon for the active one
-				if ( props?.attributes?.className !== undefined && props.attributes.className.includes(`align${slug}`) ) {
-					currentIcon = blockAlignIcon( icon );
+				// disable specific blocks from having custom alignment support
+				if (
+					excludeBlocksFromCustomAlignmentSupport.includes(
+						blockName
+					)
+				) {
+					return originalEdit;
 				}
 
-				// only add the current align control if it's supported. We should always include "none".
-				if ( ( defaultWPAlignments.find( alignment => alignment.slug === slug ) || defaultWPJustifcations.find( alignment => alignment.slug === slug ) ) && ! blockAlignSupport.includes( slug ) && slug !== 'none' ) {
-					continue;
+				// check for block align support
+				const blockAlignSupport = getBlockSupport(
+					blockName,
+					'alignCustom',
+					false
+				);
+
+				// do not add this custom controls if the block type has no align support
+				if ( ! blockAlignSupport ) {
+					return originalEdit;
 				}
 
-				// handle creating the
-				let controlTitle = width !== false && ! width.includes('%') ? (
-					<span className={'components-menu-item__item'}>
-						<span className={'components-menu-item__info-wrapper'}>
-							<span className={ 'components-menu-item__item'}>{ __( name, textdomain ) }</span>
-							<span className={'components-menu-item__info'}>{ 'Max ' + width + ' wide' }</span>
-						</span>
-					</span>
-				) : __( name, textdomain );
+				// combine default WP alignments, justications and custom alignments from theme.json
+				const combinedAlignments = [
+					...defaultWPAlignments,
+					...themeJSON.settings._experimentalLayout,
+					...defaultWPJustifcations,
+				];
 
-				let newControl = {
-					title: controlTitle,
-					icon: blockAlignIcon( icon ),
-					onClick: () => {
-						if ( slug === 'none' ) {
-							// Because we don't want a "alignnone" classname in our block
-							slug = false;
-						}
+				/*
+				 * Build the toolbar block alignment controls depening on the align support of the block type
+				 */
 
-						// set classes on the block depending on if a class is already set on the block
-						let updatedClass = '';
-						const currentClass = props?.attributes?.className;
+				let currentIcon = 'none';
+				const allowedAlignControls = [];
 
-						if ( currentClass !== undefined && currentClass.includes(' ') ) {
-							const classArray = currentClass.split( ' ' );
+				for ( const alignment of combinedAlignments ) {
+					let { name, slug, icon, width, textdomain } = alignment;
 
-							classArray.forEach( ( singleClass, index ) => {
-								if ( singleClass.includes('align') ) {
-									classArray[ index ] = `align${slug}`;
-								}
-							});
+					// check if this alignment is the currently selected alignment for the block and grab the icon for the active one
+					if (
+						props?.attributes?.className !== undefined &&
+						props.attributes.className.includes( `align${ slug }` )
+					) {
+						currentIcon = blockAlignIcon( icon );
+					}
 
-							updatedClass = classArray.join( ' ' );
-						} else {
-							updatedClass = `align${slug}`;
-						}
+					// only add the current align control if it's supported. We should always include "none".
+					if (
+						( defaultWPAlignments.find(
+							( anAlignment ) => anAlignment.slug === slug
+						) ||
+							defaultWPJustifcations.find(
+								( anAlignment ) => anAlignment.slug === slug
+							) ) &&
+						! blockAlignSupport.includes( slug ) &&
+						slug !== 'none'
+					) {
+						continue;
+					}
 
-						// Change the block align attribute
-						props.setAttributes( {
-							align: slug === undefined ? 'none' : slug,
-							className: updatedClass,
-						} );
+					// handle creating the title of the control
+					// disable eslint here because it thinks the textdomain isn't correct
+					/* eslint-disable */
+					const controlTitle =
+						width !== false && ! width.includes( '%' ) ? (
+							<span className={ 'components-menu-item__item' }>
+								<span
+									className={
+										'components-menu-item__info-wrapper'
+									}
+								>
+									<span
+										className={
+											'components-menu-item__item'
+										}
+									>
+										{ __( name, textdomain ) }
+									</span>
+									<span
+										className={
+											'components-menu-item__info'
+										}
+									>
+										{ 'Max ' + width + ' wide' }
+									</span>
+								</span>
+							</span>
+						) : (
+							__( name, textdomain )
+						);
+					/* eslint-enable */
 
-						// Change the current ToolbarDropdownMenu icon if the block align has been changed
-						const alignToolbarButton = document.querySelector('[aria-label="Align"]');
+					const newControl = {
+						title: controlTitle,
+						icon: blockAlignIcon( icon ),
+						onClick: () => {
+							if ( slug === 'none' ) {
+								// Because we don't want a "alignnone" classname in our block
+								slug = false;
+							}
 
-						if ( alignToolbarButton ) {
-							alignToolbarButton.innerHTML = `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="${icon}"/></svg>`;
-						}
-					},
-				};
+							// set classes on the block depending on if a class is already set on the block
+							let updatedClass = '';
+							const currentClass = props?.attributes?.className;
 
-				allowedAlignControls.push( newControl );
-			}
+							if (
+								currentClass !== undefined &&
+								currentClass.includes( ' ' )
+							) {
+								const classArray = currentClass.split( ' ' );
 
-			// if we didn't get a match for an icon, we'll just show the "none" option
-			if ( currentIcon === 'none' ) {
-				currentIcon = combinedAlignments.find( alignment => alignment.slug === 'none' );
-				currentIcon = blockAlignIcon( currentIcon.icon );
-			}
+								classArray.forEach( ( singleClass, index ) => {
+									if ( singleClass.includes( 'align' ) ) {
+										classArray[ index ] = `align${ slug }`;
+									}
+								} );
 
-			/*
-			* Re-Build the block toolbar and edit
-			*/
+								updatedClass = classArray.join( ' ' );
+							} else {
+								updatedClass = `align${ slug }`;
+							}
 
-			return el(
-				Fragment,
-				{},
-				el( BlockControls, {
-						key: 'controls',
-						group: 'default',
-					},
-					el( ToolbarGroup, null,
-						el( ToolbarDropdownMenu, {
-							label: 'Align',
-							icon: currentIcon,
-							controls: allowedAlignControls
-						} ),
+							// Change the block align attribute
+							props.setAttributes( {
+								align: slug === undefined ? 'none' : slug,
+								className: updatedClass,
+							} );
+
+							// Change the current ToolbarDropdownMenu icon if the block align has been changed
+							const alignToolbarButton = document.querySelector(
+								'[aria-label="Align"]'
+							);
+
+							if ( alignToolbarButton ) {
+								alignToolbarButton.innerHTML = `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="${ icon }"/></svg>`;
+							}
+						},
+					};
+
+					allowedAlignControls.push( newControl );
+				}
+
+				// if we didn't get a match for an icon, we'll just show the "none" option
+				if ( currentIcon === 'none' ) {
+					currentIcon = combinedAlignments.find(
+						( alignment ) => alignment.slug === 'none'
+					);
+					currentIcon = blockAlignIcon( currentIcon.icon );
+				}
+
+				/*
+				 * Re-Build the block toolbar and edit
+				 */
+
+				return el(
+					Fragment,
+					{},
+					el(
+						BlockControls,
+						{
+							key: 'controls',
+							group: 'default',
+						},
+						el(
+							ToolbarGroup,
+							null,
+							el( ToolbarDropdownMenu, {
+								label: 'Align',
+								icon: currentIcon,
+								controls: allowedAlignControls,
+							} )
+						)
 					),
-				),
-				el( BlockEdit, props )
-			);
-		};
-	}, 'withInspectorControls' );
+					el( BlockEdit, props )
+				);
+			};
+		},
+		'withInspectorControls'
+	);
 
-	wp.hooks.addFilter( 'editor.BlockEdit', 'tribe/with-inspector-controls', customBlockAlignmentControls );
-
-})();
+	wp.hooks.addFilter(
+		'editor.BlockEdit',
+		'tribe/with-inspector-controls',
+		customBlockAlignmentControls
+	);
+} )();
