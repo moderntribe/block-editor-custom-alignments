75c556b5ec32adb74501021151bc5761e28b5ba9
Mykhailo Los <mlkilderkin@gmail.com>
2025-04-22 12:40:12 +0300

    Resolve PHPstan notice for non-empty variable

--- block-editor-custom-alignments.php
+++ block-editor-custom-alignments.php
@@ -154,7 +154,7 @@ class Block_Editor_Custom_Alignments {
 		if ( $json_theme_content !== false ) {
 			$json_theme_json = $this->get_json_theme_json( $json_theme_content );
 
-			if ( isset( $this->theme_json->settings->_experimentalLayout ) ) {
+			if ( isset( $this->theme_json->settings ) ) {
 				return  $json_theme_json;
 			}
 		}
